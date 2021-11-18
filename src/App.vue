89078b39c82900b1c68dc3adee79b7c40e100230
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-11-18 15:19:41 +0100

    print poster

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,48 @@
   <div id="app">
     
     <div class="ps-3">
-      <input type="text" placeholder="Inserisci testo..." id="search" v-model="textToSearch" @keyup.enter="searchQuery">
-      <button type="button" @click="searchQuery">Cerca</button>
+      <input type="text" placeholder="Inserisci testo..." id="search" v-model="textToSearch" @keyup.enter="filter">
+      <button type="button" @click="filter">Cerca</button>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <h3>Movies</h3>
+        <Card v-for="movie in movies" :key="movie.id" :data="movie"></Card>
+      </div>
+
+      <div class="col">
+        <h3>Series</h3>
+        <Card v-for="serie in series" :key="serie.id" :data="serie"></Card>
+      </div>
     </div>
 
 
-    <ul v-for="(searchCategory, i) in category" :key="i">
+    <!-- <div class="row">
+      <div class="col">
+        <h3>Movie</h3>
+        <ul v-for="movie in movies" :key="movie.id">
+          <li><strong>Titolo:</strong> {{movie.title}}</li>
+          <li><strong>Titolo originale:</strong> {{movie.original_title}}</li>
+          <li><strong>Lingua:</strong><img class="_lang" alt="lang" :src="langFlags[movie.original_language]"></li>
+          <li><strong>Voto:</strong> {{movie.vote_average}}</li>
+          <hr>
+        </ul>
+      </div>
+
+      <div class="col"> 
+        <h3>Serie</h3>
+        <ul v-for="serie in series" :key="serie.id">
+          <li><strong>Titolo:</strong> {{serie.name}}</li>
+          <li><strong>Titolo originale:</strong> {{serie.original_name}}</li>
+          <li><strong>Lingua:</strong><img class="_lang" alt="lang" :src="langFlags[serie.original_language]"></li>
+          <li><strong>Voto:</strong> {{serie.vote_average}}</li>
+          <hr>
+        </ul>
+      </div>
+    </div> -->
+
+    <!-- <ul v-for="(searchCategory, i) in category" :key="i">
       <ul v-for="el in searchCategory" :key="el.id">
 
         <li>
@@ -27,36 +63,38 @@
         <hr>
 
       </ul>
-    </ul>
+    </ul> -->
 
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Card from "./components/Card.vue"
 
 export default {
   name: 'App',
-  components: {},
+  components: {Card,},
   data() {
     return{
       apiKey: "04ca2bb9b036573f160499e80eb6b10a",
       apiUrl: "https://api.themoviedb.org/3",
       textToSearch: "",
-      category: {
-        movies: [],
-        series: [],
-      },
+      movies: [],
+      series: [],
       langFlags: {
-        en: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/280px-Flag_of_the_United_States_%28Pantone%29.svg.png",
+        en: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png",
         it: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/280px-Flag_of_Italy.svg.png",
         es: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/280px-Flag_of_Spain.svg.png",
+        de: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/280px-Flag_of_Germany.svg.png",
+        fr: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931958%29.svg/280px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931958%29.svg.png",
       },
     }
   },
 
 
   methods: {
+    /*
     api(categorySearch) {
       axios.get(this.apiUrl + "/search/" + categorySearch, {
       params:{
@@ -78,7 +116,24 @@ export default {
       this.api('tv')
       this.keyWord = ""
     },
+    */
+   
+    searchQuery(url, textToSearch, dataKey) {
+      axios.get(this.apiUrl + url, {
+        params: {
+          api_key: this.apiKey,
+          query: textToSearch,
+          language: "it",
+        },
+      }).then((resp) => {
+          this[dataKey] = resp.data.results;
+      });
+    },
 
+    filter() {
+      this.searchQuery("/search/movie", this.textToSearch, "movies");
+      this.searchQuery("/search/tv", this.textToSearch, "series");
+    },
   },
 
 
---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ul>
+      <li><img :src="imgPath" alt="" class="_poster"></li>
+      <li>{{ title }}</li>
+      <li v-if="this.title != this.original">{{ original }}</li>
+      <li>{{ flagPath }}</li>
+    </ul>
+
+  
+    <!-- <img :src="flagPath" alt="" /> -->
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object,
+  },
+  computed: {
+     imgPath(){
+       const pathRoot = "https://image.tmdb.org/t/p/";
+       const imxSize = "w342";
+
+       if(!this.data.poster_path){
+         return require("@/assets/Movie-placeholder.png")
+       }
+
+       return pathRoot + imxSize + this.data.poster_path;
+     },
+
+     title(){
+       return this.data.title ? this.data.title : this.data.name
+     },
+
+     originalTitle(){
+       return this.data.original_title ? this.data.original_title : this.data.original_name
+     },
+
+
+    flagPath(){
+      return this.data.orginal_language
+    },
+
+  }
+}
+</script>
