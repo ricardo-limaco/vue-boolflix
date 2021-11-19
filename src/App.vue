f0a8cbe84dd167a756647198c481607eb5728be7
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-11-19 16:26:07 +0100

    Vote + Flags

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="app">
     
-    <div class="ps-3">
-      <input type="text" placeholder="Inserisci testo..." id="search" v-model="textToSearch" @keyup.enter="filter">
-      <button type="button" @click="filter">Cerca</button>
-    </div>
+    <header>
+      <nav class="d-flex justify-content-between align-items-center">
+        <h1>BOOLFLIX</h1>
+        <div class="input-group mb-3 _input-search">
+          <input
+          type="text" class="form-control" placeholder="Inserisci testo..."
+          aria-label="Recipient's username" aria-describedby="button-addon2" id="search"
+          v-model="textToSearch" @keyup.enter="filter">
+          <button class="btn btn-danger" type="button" id="button-addon2" @click="filter">Cerca</button>
+        </div>
+      </nav>
+    </header>
+
+
+    <main>
+      
+
+      
+    </main>
+
 
     <div class="row">
       <div class="col">
@@ -18,52 +34,6 @@
       </div>
     </div>
 
-
-    <!-- <div class="row">
-      <div class="col">
-        <h3>Movie</h3>
-        <ul v-for="movie in movies" :key="movie.id">
-          <li><strong>Titolo:</strong> {{movie.title}}</li>
-          <li><strong>Titolo originale:</strong> {{movie.original_title}}</li>
-          <li><strong>Lingua:</strong><img class="_lang" alt="lang" :src="langFlags[movie.original_language]"></li>
-          <li><strong>Voto:</strong> {{movie.vote_average}}</li>
-          <hr>
-        </ul>
-      </div>
-
-      <div class="col"> 
-        <h3>Serie</h3>
-        <ul v-for="serie in series" :key="serie.id">
-          <li><strong>Titolo:</strong> {{serie.name}}</li>
-          <li><strong>Titolo originale:</strong> {{serie.original_name}}</li>
-          <li><strong>Lingua:</strong><img class="_lang" alt="lang" :src="langFlags[serie.original_language]"></li>
-          <li><strong>Voto:</strong> {{serie.vote_average}}</li>
-          <hr>
-        </ul>
-      </div>
-    </div> -->
-
-    <!-- <ul v-for="(searchCategory, i) in category" :key="i">
-      <ul v-for="el in searchCategory" :key="el.id">
-
-        <li>
-          <span v-if="i === 'movies'"><strong>Genere:</strong> Movie</span>
-          <span v-if="i === 'series'"><strong>Genere:</strong> Serie</span>
-        </li>
-        <li>
-          <span v-if="i === 'movies'"><strong>Titolo:</strong> {{el.title}}</span>
-          <span v-if="i === 'series'"><strong>Titolo:</strong> {{el.name}}</span>
-        </li>
-        <li>
-          <span v-if="i === 'movies'"><strong>Titolo originale:</strong> {{el.original_title}}</span>
-          <span v-if="i === 'series'"><strong>Titolo originale:</strong> {{el.original_name}}</span>
-        </li>
-        <li><strong>Lingua:</strong><img class="_lang" alt="lang" :src="langFlags[el.original_language]"></li>
-        <li><strong>Voto:</strong> {{el.vote_average}}</li>
-        <hr>
-
-      </ul>
-    </ul> -->
 
   </div>
 </template>
@@ -82,41 +52,12 @@ export default {
       textToSearch: "",
       movies: [],
       series: [],
-      langFlags: {
-        en: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png",
-        it: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/280px-Flag_of_Italy.svg.png",
-        es: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/280px-Flag_of_Spain.svg.png",
-        de: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/280px-Flag_of_Germany.svg.png",
-        fr: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931958%29.svg/280px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931958%29.svg.png",
-      },
     }
   },
 
 
   methods: {
-    /*
-    api(categorySearch) {
-      axios.get(this.apiUrl + "/search/" + categorySearch, {
-      params:{
-        api_key: this.apiKey,
-        query: this.textToSearch,
-        language: "it",
-      },
-    }).then((resp) =>{
-      if (categorySearch === "movie"){
-        this.category.movies = resp.data.results
-      } else if (categorySearch === "tv") {
-        this.category.series = resp.data.results
-      }
-    })
-    },
 
-    searchQuery() {
-      this.api('movie')
-      this.api('tv')
-      this.keyWord = ""
-    },
-    */
    
     searchQuery(url, textToSearch, dataKey) {
       axios.get(this.apiUrl + url, {
@@ -138,21 +79,7 @@ export default {
 
 
   mounted() {
-    // this.searchQuery("/search/movie", this.textToSearch, "movies")
 
-    //Tutte le chiamate devono avere apiKey
-    //Serch deve avere anche "query"
-
-    /*
-    axios.get(this.apiUrl + "/search/movie",{
-      params:{
-        api_key: this.apiKey,
-        query: "strange",
-      },
-    }).then((resp) =>{
-      this.movies = resp.data.results;
-    })
-    */
   }
 }
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,22 +2,48 @@
   <div>
     <ul>
       <li><img :src="imgPath" alt="" class="_poster"></li>
-      <li>{{ title }}</li>
-      <li v-if="this.title != this.original">{{ originalTitle }}</li>
-      <li>{{ flagPath }}</li>
+      <li>Titolo: {{ title }}</li>
+      <li v-if="originalTitle">Titolo originale: {{ originalTitle }}</li>
+      <li>Bandiera: <img :src="flags" alt="" class="_lang"></li>
+      <li v-if="this.data.overview"><p>Testo: {{ this.data.overview }}</p></li>
+
+
+      <li>
+        <strong>Voto: </strong>
+        <div class="stars-container" style="display: inline-block">
+          <i
+            v-for="num in 5" :key="'vote_star_' + num"
+            class="fa" :class="num <= vote ? 'fa-star' : 'fa-star-o'"
+          ></i>
+        </div>
+      </li>
+
     </ul>
 
   
-    <!-- <img :src="flagPath" alt="" /> -->
+
   </div>
 
 </template>
 
 <script>
 export default {
+  data() {
+    return{
+      langFlags: {
+        en: require("svg-country-flags/png100px/sh.png"),
+        it: require("svg-country-flags/png100px/it.png"),
+        es: require("svg-country-flags/png100px/es.png"),
+        de: require("svg-country-flags/png100px/de.png"),
+        fr: require("svg-country-flags/png100px/fr.png"),
+        ja: require("svg-country-flags/png100px/jp.png"),
+      },
+    }
+  },
   props: {
     data: Object,
   },
+
   computed: {
      imgPath(){
        const pathRoot = "https://image.tmdb.org/t/p/";
@@ -35,13 +61,27 @@ export default {
      },
 
      originalTitle(){
-       return this.data.original_title ? this.data.original_title : this.data.original_name
+       const orginal = this.data.original_title ? this.data.original_title : this.data.original_name;
+
+       return this.title === orginal ? "" : orginal
      },
 
-
-    flagPath(){
-      return this.data.orginal_language
+    flags(){
+      if(!this.langFlags[this.data.original_language]){
+        return require("@/assets/Flag-placeholder.png")
+      }
+      return this.langFlags[this.data.original_language]
     },
+
+    vote() {
+      return Math.round(this.data.vote_average / 2);
+    },
+
+  },
+
+
+  methods: {
+ 
 
   }
 }
